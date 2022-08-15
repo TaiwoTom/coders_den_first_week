749670df1f32470688673c40b93e7562461f5e11
Taiwo Tom-Ayegunle <taiwotom-ayegunle@Taiwos-MacBook-Pro.local>
2022-08-15 04:07:39 +0100

    First git

---
+++ variable.js
@@ -0,0 +1,30 @@
+/* Question 1-5 were answered during the online lesson and the ones that involve creation of new files were done through the files been worked on currently */
+
+// question 6:  Declare four variables without assigning values
+let v1;
+let v2;
+let v3;
+let v4;
+ 
+// question 7:  Declare four variables with assigning values
+let v1 = "nine"; //string
+let v2 = 9; //number
+let v3 = true; // boolean
+let v4 = null; //null
+
+/* question 8 :  Declare variable to store first name, last name, marital status,  country and age in multiple lines */
+let firstName = "Taiwo";
+let lastName = "Tom";
+let maritalStatus = "Married";
+let countryOfOrigin = "America";
+let age = 18;
+
+/* question 9 :  Declare variable to store first name, last name, marital status,  country and age in a single line */
+
+let firstName = "Taiwo", lastName = "Tom", maritalStatus = "Married", countryOfOrigin = "America", let age = 18;
+
+/* question 10: Declare two variables myAge and yourAge and assign them initial values and log to the browser console*/
+let myAge = 18;
+let yourAge = 12;
+console.log(myAge);
+console.log(yourAge);
